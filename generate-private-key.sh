75acd70394b9367bef1b5bf53cc53b7c69b3b1e8
Scott Rigby <scott@r6by.com>
2019-09-24 12:18:22 -0400

    Output fingerprint in md5 format for use in CircleCI add_ssh_keys step
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- generate-private-key.sh
+++ generate-private-key.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 temp=$(mktemp -d)
-echo temp=$temp
 
 # Generate CircleCI-compatible ssh key
 # Ref: https://circleci.com/docs/2.0/add-ssh-key/
-ssh-keygen -m PEM -t rsa -C "your_email@example.com" -f $temp/id_rsa -N ''
+ssh-keygen -E md5 -m PEM -t rsa -C "your_email@example.com" -f $temp/id_rsa -N ''
 
 cat $temp/id_rsa*
 rm -r $temp
